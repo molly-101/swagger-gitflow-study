6508cbeadd18dea0c36e9afa5458007ade33e2aa
onAuspicious <wotj7687@icloud.com>
2022-01-12 20:13:19 +0900

    Swagger OAS 3 설정

---
+++ src/main/java/swagger/swaggerstudy/controller/BasicController.java
@@ -0,0 +1,20 @@
+package swagger.swaggerstudy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @GetMapping("/api/hello1")
+    public String hello1() {
+        return "hello";
+    }
+
+    @GetMapping("/api/hello2")
+    public String hello2(@RequestParam String param) {
+        return param;
+    }
+}
