936b5991edc45574cc9b39ded521b45ec54a7d4f
onAuspicious <wotj7687@icloud.com>
2022-01-12 18:58:57 +0900

    Initial commit

---
+++ src/main/java/swagger/swaggerstudy/SwaggerStudyApplication.java
@@ -0,0 +1,13 @@
+package swagger.swaggerstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swagger/swaggerstudy/SwaggerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package swagger.swaggerstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
